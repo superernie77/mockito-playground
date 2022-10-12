370c20a56f8c866edcac96054395057152d008b7
superernie77 <superernie77@gmail.com>
2022-10-12 10:02:08 +0200

    verify and thenThrow examples. Java version to 18

---
+++ mockito-playground/src/test/java/se77/BasicClassErrorTest.java
@@ -0,0 +1,43 @@
+package se77;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// Example on how to test error/exception cases with Mockito.
+@ExtendWith(MockitoExtension.class)
+public class BasicClassErrorTest {
+
+	// Mock the BasicService class. It is an dependency used in the BasicClass.
+	@Mock
+	BasicService service;
+
+	// Inject the mock into the BasicClass we want to test.
+	@InjectMocks
+	BasicClass classToTest;
+
+	// Setup the behavior of the mocked class.
+	@BeforeEach
+	public void setup() {
+		// Throw an RuntimeException when the doComplexStuff-method is called on the
+		// mock. By this you can test correct error handling of your test class without
+		// the need of causing a real exception.
+		Mockito.when(service.doComplexStuff()).thenThrow(new RuntimeException());
+	}
+
+	// Test the method of the class
+	@Test
+	public void testDoStuffWithException() {
+
+		// call the method and tell JUnit 5 to expect a RuntimeException.
+		assertThrows(RuntimeException.class, () -> classToTest.doStuff());
+	}
+
+}
--- mockito-playground/src/test/java/se77/BasicClassTest.java
+++ mockito-playground/src/test/java/se77/BasicClassTest.java
@@ -13,31 +13,45 @@ import org.mockito.junit.jupiter.MockitoExtension;
 // This is an example for the simplest test with a mocked class. 
 @ExtendWith(MockitoExtension.class)
 public class BasicClassTest {
-	
-	// mock the BasicService class. This one is used inside the BasicClass.
+
+	// mock the BasicService class. It is an dependency used in the BasicClass.
 	@Mock
 	BasicService service;
-	
+
 	// Inject the mock into the class we want to test
 	@InjectMocks
 	BasicClass classToTest;
-	
-	 // Setup the behavior of the mocked class
+
+	// Setup the behavior of the mocked class
 	@BeforeEach
 	public void setup() {
-		// Return the String "mocked!" when the doComplexStuff-method is called on the mock.
+		// Return the String "mocked!" when the doComplexStuff-method is called on the
+		// mock. This is called "stubbing".
 		Mockito.when(service.doComplexStuff()).thenReturn("mocked!");
 	}
-	
+
 	// Test the method of the class
-	@Test 
-	public void testDoStuff() {
-		
+	@Test
+	public void testDoStuffWithJunitAssert() {
+
 		// call the method
 		String result = classToTest.doStuff();
-		
-		// ... and verify the result. It contains the value we defined in the setup-method.
+
+		// ... and verify the result. It contains the value we defined in the
+		// setup-method.
 		assertEquals("mocked!", result);
-		
+
+	}
+
+	// Tests that the service method has been called. This is similar to the first test, but 
+	// checks the correct behavior of the content of the BasicClass.
+	@Test
+	public void testDoStuffWithMockitoVerify() {
+
+		// call the method
+		classToTest.doStuff();
+
+		// ... and verify that the service-method has been called exactly once. 
+		Mockito.verify(service).doComplexStuff();
 	}
 }
