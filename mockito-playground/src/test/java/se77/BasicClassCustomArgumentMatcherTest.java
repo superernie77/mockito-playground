0c1a9d41635ffdf0639866f9b6cbd468c806bf2b
superernie77 <superernie77@gmail.com>
2022-10-18 12:57:23 +0200

    custom Argument Matchers

--- mockito-playground/src/test/java/se77/BasicClassCustomArgumentMatcherTest.java
+++ mockito-playground/src/test/java/se77/BasicClassCustomArgumentMatcherTest.java
@@ -1,6 +1,7 @@
 package se77;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -12,34 +13,38 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-//Example for the use of ArgumentMatchers. These canbe used in stubbing or for test verifiction.
+//Example for the use of custom ArgumentMatchers in the verification part of the test. 
 //For more info on Mockito check the Javadocs https://javadoc.io/static/org.mockito/mockito-core/3.12.4/org/mockito/Mockito.html
 @ExtendWith(MockitoExtension.class)
-public class BasicClassArgumentMatcherTest {
-	
+public class BasicClassCustomArgumentMatcherTest {
+
 	// Mock the BasicService class. It is an dependency used in the BasicClass.
 	@Mock
-	BasicService service;
+	private BasicService service;
 
 	// Inject the mock into the BasicClass we want to test.
 	@InjectMocks
-	BasicClass classToTest;
-	
-	
+	private BasicClass classToTest;
+
 	@BeforeEach
 	public void setup() {
-		// This defines the accepted parameter values of the method. In this case any of the given types are accepted.
+		// This defines the accepted parameter values of the method. In this case any of
+		// the given types are accepted.
 		when(service.doStuffWithParameters(anyString(), any(Integer.class), anyBoolean())).thenReturn("mocked");
 	}
-	
+
 	@Test
 	public void testDoStuffWithParameters() {
 		String result = service.doStuffWithParameters("test", 5, false);
-		
+
 		assertEquals("mocked", result);
-		
-		// verify that the method has been called by the exact parameters with ArgumentMatchers 
-		verify(service).doStuffWithParameters( eq("test") ,  eq( Integer.valueOf(5)) , eq(false));
+
+		// verify that the method has been called by the exact parameters with
+		// cuatom ArgumentMatchers
+		verify(service).doStuffWithParameters( 
+				argThat( s -> s.equals("test")), // inside the Lambda anything can be checked that evaluates to a boolean for the given type  
+				argThat( i -> i.equals(5)), 
+				booleanThat(b -> b.equals(false)) ); // there is a version for any primitive type
 	}
 
 }
