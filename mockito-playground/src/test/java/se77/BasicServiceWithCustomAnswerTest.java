81b3feee8574235fecc3aa9388ff5617784e822b
superernie77 <superernie77@gmail.com>
2022-10-26 09:54:29 +0200

    Custom answer example

---
+++ mockito-playground/src/test/java/se77/BasicServiceWithCustomAnswerTest.java
@@ -0,0 +1,38 @@
+package se77;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// Example for a custom return value for a method invocation
+@ExtendWith(MockitoExtension.class)
+public class BasicServiceWithCustomAnswerTest {
+	
+	@Mock
+	private BasicService service;
+	
+	@BeforeEach
+	public void setup() {
+		
+		when(service.doStuffWithParameters(anyString(), anyInt(), anyBoolean())).thenAnswer( invocation -> {
+			BasicService mock = (BasicService)invocation.getMock();
+			String arg1 = (String)invocation.getArgument(0);
+			return "Answer of mock: "+mock.toString()+" , first argument is: "+arg1;
+		});
+	}
+	
+	@Test
+	public void testCustomAnswer() {
+		String result = service.doStuffWithParameters("one", 2, false);
+		assertEquals("Answer of mock: service , first argument is: one", result);
+	}
+
+}
